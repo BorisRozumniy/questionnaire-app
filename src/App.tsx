35d3a458a8574736b509e74cb6467e1402182345
risbo <b.rozumnyi@ringostat.com>
2022-09-14 15:45:56 +0300

    added the todoContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,18 @@
-import {
-  createContext,
-  Dispatch,
-  PropsWithChildren,
-  SetStateAction,
-  useState,
-} from "react";
-import { useContext } from "react";
+// App.tsx
+// import * as React from 'react'
+import TodoProvider from './context/todoContext'
+import Todos from './containers/Todos'
+import AddTodo from './components/AddTodo'
+// import './styles.css'
 
-
-export function createCtx<A>(defaultValue: A) {
-  type UpdateType = Dispatch<SetStateAction<typeof defaultValue>>;
-  const defaultUpdate: UpdateType = () => defaultValue;
-  const ctx = createContext({
-    state: defaultValue,
-    update: defaultUpdate,
-  });
-
-  function Provider(props: PropsWithChildren<{}>) {
-    const [state, update] = useState(defaultValue);
-    return <ctx.Provider value={{ state, update }} {...props} />;
-  }
-  return [ctx, Provider] as const; // alternatively, [typeof ctx, typeof Provider]
-}
-
-// usage
-// import { useContext } from "react";
-
-const [ctx, TextProvider] = createCtx("someText");
-export const TextContext = ctx;
 export default function App() {
   return (
-    <TextProvider>
-      <Component />
-    </TextProvider>
-  );
-}
-export function Component() {
-  const { state, update } = useContext(TextContext);
-  return (
-    <label>
-      {state}
-      <input type="text" onChange={(e) => update(e.target.value)} />
-    </label>
-  );
+    <TodoProvider>
+      <main className='App'>
+        <h1>My Todos</h1>
+        <AddTodo />
+        <Todos />
+      </main>
+    </TodoProvider>
+  )
 }
