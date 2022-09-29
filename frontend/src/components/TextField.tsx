7878d549683c1d1584444d8fe5def8e5d6417f07
risbo <b.rozumnyi@ringostat.com>
2022-09-29 23:17:50 +0300

    updating a question in the TextField

--- frontend/src/components/TextField.tsx
+++ frontend/src/components/TextField.tsx
@@ -1,6 +1,39 @@
-import { FC } from "react";
+import { FC, FormEvent, useContext, useState } from "react";
+import {
+  ContextType,
+  IQuestion,
+  QuestionItemContextType,
+} from "../@types/question";
+import { saveEditedQuestion } from "../actions/saveEditedQuestion";
+import { Context } from "../context/context";
+import { QuestionItemContext } from "../context/questionItemContext";
 import { Input } from "./Styled/Input";
 
-export const TextField: FC<{ editMod: boolean }> = ({ editMod }) => (
-  <Input disabled={editMod} />
-);
+type onChangeT = (e: FormEvent<HTMLInputElement>) => void;
+
+type Props = {
+  editMod: boolean;
+};
+
+export const TextField: FC<Props> = ({ editMod }) => {
+  const { questions, setQuestions } = useContext(Context) as ContextType;
+
+  const { question } = useContext(
+    QuestionItemContext
+  ) as QuestionItemContextType;
+
+  const [value, setValue] = useState(question.userAnswer);
+
+  const onChange: onChangeT = ({ currentTarget }) =>
+    setValue(currentTarget.value);
+
+  const onBlur = (): void => {
+    const questionUpdate: IQuestion = {
+      ...question,
+      userAnswer: value,
+    };
+    saveEditedQuestion(questionUpdate, questions, setQuestions);
+  };
+
+  return <Input {...{ disabled: editMod, onChange, value, onBlur }} />;
+};
