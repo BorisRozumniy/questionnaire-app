898922df1b2bc90868ab0d8741416eab2f1bec39
risbo <b.rozumnyi@ringostat.com>
2022-10-19 15:02:10 +0300

    frontend: save user's answer for the TextField

--- frontend/src/components/TextField.tsx
+++ frontend/src/components/TextField.tsx
@@ -1,36 +1,51 @@
 import { FC, FormEvent, useContext, useState } from "react";
-// import { ContextType } from "../@types/context";
-import { IQuestion, QuestionItemContextType } from "../@types/question";
-// import { saveEditedQuestion } from "../actions/saveEditedQuestion";
-// import { Context } from "../context/context";
+import { useParams } from "react-router-dom";
+import { ContextType } from "../@types/context";
+import { QuestionItemContextType } from "../@types/question";
+import { TUserAnswer } from "../@types/respondent";
+import { patchRequestChangeRespondentAnswer } from "../actions/patchRequestChangeRespondentAnswer";
+import { Context } from "../context/context";
 import { QuestionItemContext } from "../context/questionItemContext";
 import { Input } from "./Styled/Input";
 
 type onChangeT = (e: FormEvent<HTMLInputElement>) => void;
 
 export const TextField: FC = () => {
-  // const { editMod, questions, setQuestions } = useContext(
-  //   Context
-  // ) as ContextType;
+  const { respondentsState, respondentsDispatch } = useContext(
+    Context
+  ) as ContextType;
 
   const { question } = useContext(
     QuestionItemContext
   ) as QuestionItemContextType;
 
-  const [value, setValue] = useState(question.userAnswer);
+  let params = useParams();
+  const respondentId = params.id!.substring(1);
+  const respondent = respondentsState.respondents.find(
+    (item) => item._id === respondentId
+  );
+
+  const [value, setValue] = useState(
+    respondent?.answers?.find((answer) => answer.questionId === question._id)
+      ?.value || ""
+  );
 
   const onChange: onChangeT = ({ currentTarget }) =>
     setValue(currentTarget.value);
 
   const onBlur = (): void => {
-    if (question.userAnswer !== value) {
-      const questionUpdate: IQuestion = {
-        ...question,
-        userAnswer: value,
-      };
-      // saveEditedQuestion(questionUpdate, questions, setQuestions);
+    const newUserAnswer: TUserAnswer = {
+      questionId: question._id,
+      value: value,
+    };
+    if (respondent?.answers) {
+      patchRequestChangeRespondentAnswer({
+        requestBody: newUserAnswer,
+        respondentId,
+        dispatch: respondentsDispatch,
+      });
     }
   };
 
-  return <Input {...{ /* disabled: editMod, */ onChange, value, onBlur }} />;
+  return <Input {...{ onChange, value, onBlur }} />;
 };
