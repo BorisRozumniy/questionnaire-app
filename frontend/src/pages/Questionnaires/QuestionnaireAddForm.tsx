61da5f699de39381e8fbcd06c34fb5af2df153cf
risbo <b.rozumnyi@ringostat.com>
2022-11-10 17:39:11 +0100

    frontend: styled the QuestionnaireAddForm

--- frontend/src/pages/Questionnaires/QuestionnaireAddForm.tsx
+++ frontend/src/pages/Questionnaires/QuestionnaireAddForm.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, FC, FormEvent, SetStateAction, useState } from "react";
+import { Dispatch, FC, FormEvent, useState } from "react";
 import styled from "styled-components";
 import {
   QUESTIONNAIRES_ACTIONTYPE,
@@ -13,9 +13,9 @@ type Props = {
   questionnaireDispatch: Dispatch<QUESTIONNAIRES_ACTIONTYPE>;
 };
 
-export const QuestionnaireAddForm: FC<Props> = ({
-  questionnaireDispatch,
-}) => {
+export const QuestionnaireAddForm: FC<Props> = ({ questionnaireDispatch }) => {
+  const [isShown, setIsShown] = useState(false);
+
   const [value, setValue] = useState("");
 
   const onChange = ({ currentTarget }: FormEvent<HTMLInputElement>): void => {
@@ -35,13 +35,32 @@ export const QuestionnaireAddForm: FC<Props> = ({
     });
   };
 
+  const toggleShow = () => setIsShown(!isShown);
+
+  if (!isShown) return <ShowButton onClick={toggleShow}>+</ShowButton>;
+
   return (
     <Wrapper>
-      <Input {...{ onChange, value }} />
-      <Button {...{ onClick }}>New Questionnaire</Button>
+      <CloseButton onClick={toggleShow} bg="red">
+        x
+      </CloseButton>
+      <H2>Add new Questionnaire</H2>
+      <Form>
+        <Input {...{ onChange, value }} />
+        <Button {...{ onClick }}>New Questionnaire</Button>
+      </Form>
     </Wrapper>
   );
 };
+
+const ShowButton = styled(Button)`
+  position: fixed;
+  bottom: 32px;
+  right: 32px;
+  height: 60px;
+  width: 60px;
+  border-radius: 50%;
+`;
 
 const Wrapper = styled.div`
   margin: 0 auto;
@@ -50,6 +69,21 @@ const Wrapper = styled.div`
   min-width: 280px;
   max-width: 500px;
   padding: 16px;
+  position: relative;
+`;
+
+const CloseButton = styled(Button)`
+  position: absolute;
+  top: 8px;
+  right: 8px;
+`;
+
+const H2 = styled.h2`
+  margin-bottom: 8px;
+  text-align: center;
+`;
+
+const Form = styled.div`
   display: grid;
   grid-gap: 8px;
   @media only screen and (min-width: 620px) {
